66544ab9fc785b783861de28aea0f8767b425abe
julfisu80-web <julfisu80@gmail.com>
2025-12-05 17:33:24 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,31 @@
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installed");
+  console.log("Service Worker instalado");
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated");
+  console.log("Service Worker activado");
+  return self.clients.claim();
 });
 
+// Estrategia simple: permitir que la app funcione offline con cache básico
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.open("cho-cache").then((cache) =>
+      cache.match(event.request).then((response) => {
+        const fetchPromise = fetch(event.request)
+          .then((networkResponse) => {
+            if (event.request.method === "GET") {
+              cache.put(event.request, networkResponse.clone());
+            }
+            return networkResponse;
+          })
+          .catch(() => response);
+
+        return response || fetchPromise;
+      })
+    )
+  );
 });
+
 
