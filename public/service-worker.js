2f7c78629d7d5f638457307969618e25b20421cd
julfisu80-web <julfisu80@gmail.com>
2025-12-05 17:25:37 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,13 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
 
